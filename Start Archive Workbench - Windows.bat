fd1440ad66ace9b8382707c5d8b5d8c314b2238e
Alex Colman <alexdcolman@gmail.com>
2026-09-09 11:14:46 -0300

    Prepare Archive Workbench 1.0.0

--- Start Archive Workbench - Windows.bat
+++ Start Archive Workbench - Windows.bat
@@ -7,7 +7,7 @@ set "AW_PROFILE=cpu"
 if defined ARCHIVE_WORKBENCH_CPU_IMAGE (
   set "AW_IMAGE=%ARCHIVE_WORKBENCH_CPU_IMAGE%"
 ) else (
-  set "AW_IMAGE=ghcr.io/alexdcolman/archive-workbench:1.0.0-rc1-cpu"
+  set "AW_IMAGE=ghcr.io/alexdcolman/archive-workbench:1.0.0-cpu"
 )
 set "ARCHIVE_WORKBENCH_CPU_IMAGE=%AW_IMAGE%"
 
